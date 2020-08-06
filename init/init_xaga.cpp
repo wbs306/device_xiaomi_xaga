18787aaf61c429d9a382e5527c9387f3ada2a7fb
faham1997 <nafidfaham08@gmail.com>
2020-08-06 21:29:35 +0600

    xaga: allocate heap values dynamically * reference to https://github.com/LineageOS/android_device_xiaomi_sdm660-common/blob/lineage-17.1/init/init_sdm660.cpp
    Co-authored-by: DD3Boh <dade.garberi@gmail.com>
    Signed-off-by: faham1997 <nafidfaham08@gmail.com>

--- init/init_xaga.cpp
+++ init/init_xaga.cpp
@@ -28,6 +28,10 @@
  */
 
 #include <stdlib.h>
+#include <fstream>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
@@ -39,6 +43,13 @@ using android::base::GetProperty;
 using android::base::SetProperty;
 using std::string;
 
+char const *heapstartsize;
+char const *heapgrowthlimit;
+char const *heapsize;
+char const *heapminfree;
+char const *heapmaxfree;
+char const *heaptargetutilization;
+
 void property_override(string prop, string value)
 {
     auto pi = (prop_info *)__system_property_find(prop.c_str());
@@ -49,8 +60,50 @@ void property_override(string prop, string value)
         __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
 }
 
+void check_device()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > 5072ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xxhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    }
+}
+
 void vendor_load_properties()
 {
+    // dalvik
+    check_device();
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
     string model;
     string marketname;
 
