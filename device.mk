08b1a329997f9721b4eb3f6f93c0fc76f5c19fc3
SagarMakhar <sagarmakhar@gmail.com>
2022-04-22 16:43:38 +0000

    xaga: Import lights aidl from xiaomi_sm8250-common
    Signed-off-by: Tkpointz <pointzztk@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -100,6 +100,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.xiaomi_mt6895
+
 # MTK plpath utils
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/mtk_plpath_utils:$(TARGET_COPY_OUT_SYSTEM)/bin/mtk_plpath_utils \
