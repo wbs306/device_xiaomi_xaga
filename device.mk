5fd1b443489bed81c223667d8c1df900da07765a
TTTT555 <albukerko@gmail.com>
2022-05-18 01:11:28 +0200

    xaga: Build FM Radio with MTK adaptation
    Include in build FM Radio app with mtk adaptation,
    pick required for work:
    https://review.lineageos.org/c/LineageOS/android_frameworks_av/+/325606
    
    Also look at the kernel and vendor tree to find the continuation of the addon
    
    Co-authored-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
+# FM Radio
+PRODUCT_PACKAGES += \
+    MtkFMRadio
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
