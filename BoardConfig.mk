ce2feda2be384b7941238f85b35adb0ebba579c1
wbs306 <wbs306@gmail.com>
2022-07-10 18:25:24 +0800

    xaga: sepolicy: Resolve misc block device denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,7 @@ VENDOR_SECURITY_PATCH := 2021-08-01
 # Sepolicy
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
