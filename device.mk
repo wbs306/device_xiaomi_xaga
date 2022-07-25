8092343d634c98c2a10955355f5c33f33d3a25b0
wbs306 <wbs306@gmail.com>
2022-07-25 22:00:10 +0800

    xaga: overlay: Build evolution overlay

--- device.mk
+++ device.mk
@@ -126,6 +126,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_PACKAGES += \
     FrameworkResOverlay \
