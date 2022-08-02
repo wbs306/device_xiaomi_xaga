63c62f3267a8c9326b9a6354795d4c8050e8064c
wbs306 <wbs306@gmail.com>
2022-08-02 17:03:53 +0800

    xaga: Build fingerprint hidl

--- device.mk
+++ device.mk
@@ -131,6 +131,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
