4af71469438a54a23b5a1459340bc0074ea463e1
wbs306 <wbs306@gmail.com>
2022-08-04 10:24:28 +0800

    xaga: symlink: Merge file list into one
    * And splict into two parts, one for xaga, one for mt6895 common

--- Android.mk
+++ Android.mk
@@ -11,21 +11,12 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-BIN_SYMLINK := $(TARGET_OUT_VENDOR)/bin/jpegtool
-BIN_SYMLINK += $(TARGET_OUT_VENDOR)/bin/dumpfaultd.mt6895
-$(BIN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf mt6895/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(BIN_SYMLINK)
-
-BIN_HW_SYMLINK := $(TARGET_OUT_VENDOR)/bin/hw/android.hardware.graphics.allocator@4.0-service-mediatek
-BIN_HW_SYMLINK += $(TARGET_OUT_VENDOR)/bin/hw/camerahalserver
-$(BIN_HW_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+ALLOCATOR_SYMLINK := $(TARGET_OUT_VENDOR)/bin/hw/android.hardware.graphics.allocator@4.0-service-mediatek
+$(ALLOCATOR_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@mkdir -p $(dir $@)
 	$(hide) ln -sf mt6895/android.hardware.graphics.allocator@4.0-service-mediatek.mt6895 $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(BIN_HW_SYMLINK)
+ALL_DEFAULT_INSTALLED_MODULES += $(ALLOCATOR_SYMLINK)
 
 MDOTA_SYMLINK := $(TARGET_OUT_VENDOR)/etc/mdota
 $(MDOTA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
@@ -34,33 +25,19 @@ $(MDOTA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(MDOTA_SYMLINK)
 
-LIB_MT6895_SYMLINK := $(addprefix $(TARGET_OUT_VENDOR)/lib/, $(strip $(shell cat $(DEVICE_PATH)/symlink/lib_mt6895.txt)))
-$(LIB_MT6895_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+XAGA_SYMLINK := $(addprefix $(TARGET_OUT_VENDOR)/, $(strip $(shell cat $(DEVICE_PATH)/symlink/xaga.txt)))
+$(XAGA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@mkdir -p $(dir $@)
 	$(hide) ln -sf mt6895/$(notdir $@) $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(LIB_MT6895_SYMLINK)
+ALL_DEFAULT_INSTALLED_MODULES += $(XAGA_SYMLINK)
 
-LIB_HW_MT6895_SYMLINK := $(addprefix $(TARGET_OUT_VENDOR)/lib/hw/, $(strip $(shell cat $(DEVICE_PATH)/symlink/lib_hw_mt6895.txt)))
-$(LIB_HW_MT6895_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+MT6895_SYMLINK := $(addprefix $(TARGET_OUT_VENDOR)/, $(strip $(shell cat $(DEVICE_PATH)/symlink/mt6895.txt)))
+$(MT6895_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@mkdir -p $(dir $@)
 	$(hide) ln -sf mt6895/$(notdir $@) $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(LIB_HW_MT6895_SYMLINK)
-
-LIB64_MT6895_SYMLINK := $(addprefix $(TARGET_OUT_VENDOR)/lib64/, $(strip $(shell cat $(DEVICE_PATH)/symlink/lib64_mt6895.txt)))
-$(LIB64_MT6895_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf mt6895/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(LIB64_MT6895_SYMLINK)
-
-LIB64_HW_MT6895_SYMLINK := $(addprefix $(TARGET_OUT_VENDOR)/lib64/hw/, $(strip $(shell cat $(DEVICE_PATH)/symlink/lib64_hw_mt6895.txt)))
-$(LIB64_HW_MT6895_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf mt6895/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(LIB64_HW_MT6895_SYMLINK)
+ALL_DEFAULT_INSTALLED_MODULES += $(MT6895_SYMLINK)
 
 AUDIO_PRIMARY_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/audio.primary.mt6895.so
 AUDIO_PRIMARY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/audio.primary.mt6895.so
@@ -77,14 +54,6 @@ $(AUDIO_R_SUBMIX_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf audio.r_submix.mediatek.so $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(AUDIO_R_SUBMIX_SYMLINK)
-
-EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/egl/libGLES_mali.so
-EGL_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/egl/libGLES_mali.so
-$(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf mt6895/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
 
 GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
 GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
