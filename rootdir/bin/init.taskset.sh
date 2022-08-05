e87404641c6eaa99e33203257bea54c3064322f2
wbs306 <wbs306@gmail.com>
2022-08-05 22:06:13 +0800

    xaga: Commonize device tree

--- rootdir/bin/init.taskset.sh
+++
@@ -1,6 +0,0 @@
-#!/vendor/bin/sh
-
-## This is a shell script for executing taskset commands at early-init step
-## Do NOT add other commands in this file otherwise you may violate its SELinux policy
-
-taskset -ap 0f `pidof -x kswapd0`
