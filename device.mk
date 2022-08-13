bbdfbbfe561180a1b807ffee0f63e4cb1ce3d150
wbs306 <wbs306@gmail.com>
2022-08-13 20:54:22 +0800

    xaga: parts: Introduce GPU optimizer
    * The GPU loading always keep in 100% whatever we do in Dimensity 8100.
    
      So I add this part into setting to limit the maximum frequency.

--- device.mk
+++ device.mk
@@ -195,7 +195,8 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 31
 
 # Xiaomi Parts
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    gpu_optimizer.rc
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/xaga/xaga-vendor.mk)
