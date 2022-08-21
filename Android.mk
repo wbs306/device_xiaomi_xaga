bf95fde663b999d36589e6e210e6acba6a15b022
wbs306 <wbs306@gmail.com>
2022-08-21 13:04:13 +0800

    xaga: Use symlink to override vendor lights service

--- Android.mk
+++ Android.mk
@@ -9,3 +9,12 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),xaga)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+
+include $(CLEAR_VARS)
+
+LIGHT_REPLACEMENT += $(TARGET_OUT_PRODUCT)/vendor_overlay/${PRODUCT_TARGET_VNDK_VERSION}/bin/hw/android.hardware.lights-service.mediatek
+$(LIGHT_REPLACEMENT): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -s /system/bin/hw/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(LIGHT_REPLACEMENT)
