8cdca09e7618d71463c3d0fddfce2cd25309bf23
wbs306 <wbs306@gmail.com>
2022-08-31 18:52:56 +0800

    xaga: Commonize proprietary-files

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,29 +8,8 @@
 
 set -e
 
-DEVICE=xaga
-VENDOR=xiaomi
+export DEVICE=xaga
+export DEVICE_COMMON=mt6895-common
+export VENDOR=xiaomi
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
-
-# Warning headers and guards
-write_headers
-
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-
-# Finish
-write_footers
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
